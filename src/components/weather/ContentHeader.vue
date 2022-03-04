8ff78b6375ba063ba8a91844a25ca7e2e8b37bb5
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-03-04 22:33:26 +0900

    Feat: "WeatherContent Header메세지 component 구현 "

---
+++ src/components/weather/ContentHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="description">
+    <h2>날씨를 알려드리겠습니다.</h2>
+    <h6>원하는 도시를 선택해주세요</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentHeader",
+};
+</script>
+
+<style scoped>
+.description {
+  padding: 1em;
+}
+.description h6 {
+  padding: 10px 0px;
+  font-size: 12px;
+}
+</style>
---
+++ src/components/weather/WeatherContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div><ContentHeader /></div>
+</template>
+
+<script>
+import ContentHeader from "./ContentHeader.vue";
+
+export default {
+  name: "WeatherContent",
+  components: {
+    ContentHeader,
+  },
+};
+</script>
+
+<style></style>
