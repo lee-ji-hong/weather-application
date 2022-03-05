e6765ca3068b2aee0d85198cd50be760f79308b7
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-03-05 17:56:00 +0900

    Refactor: API key 숨기기

--- src/mixins/weatherMixin.js
+++ src/mixins/weatherMixin.js
@@ -6,7 +6,8 @@ const weatherMixin = {
   },
   methods: {
     async getWeatherInfo(city) {
-      const API_KEY = "559ceab537cdee707f783e5c8f141a38";
+      console.log();
+      const API_KEY = process.env.VUE_APP_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.code}&appid=${API_KEY}`;
       const res = await axios.get(url);
       const { main, wind, weather } = res.data;
